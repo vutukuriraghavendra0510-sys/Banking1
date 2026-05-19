3e9fb442eb8b06697db7475081d68fc09f791165
vutukuriraghavendra0510-sys <vutukuriraghavendra0510@gmai.com>
2026-05-19 11:43:18 +0000

    ok

---
+++ Demo/src/com/bb/controller/Printer.java
@@ -0,0 +1,14 @@
+package com.bb.controller;
+
+public class Printer {
+
+	void print() {
+		System.out.println("Hello Raghagu");
+		System.out.println("Welcome to ava Programming");
+		System.out.println("Have a nice day");
+		System.out.println("Goodbye Ragh");
+		
+		System.out.println("Hello Raghagu");
+	}
+	
+}
--- Demo/src/com/bb/controller/Raghagu.java
+++ Demo/src/com/bb/controller/Raghagu.java
@@ -4,7 +4,10 @@ public class Raghagu {
 	public static void main(String[] args) {
 		System.out.println("Hello Raghagu");
 		System.out.println("Welcome to Java Programming");
+		System.out.println("Have a nice day");
+		System.out.println("Goodbye Raghagu");
 		
+		System.out.println("Hello Raghagu");
 		
 	}
 
